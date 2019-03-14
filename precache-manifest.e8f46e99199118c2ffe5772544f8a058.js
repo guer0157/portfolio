1da7857651259dca1878437892ed89368b3b8d6f
guer0157 <32390693+guer0157@users.noreply.github.com>
2019-03-14 11:23:40 -0400

    Updates

--- precache-manifest.e8f46e99199118c2ffe5772544f8a058.js
+++ precache-manifest.e8f46e99199118c2ffe5772544f8a058.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "/portfolio/static/media/tel.dca81bfb.svg"
   },
   {
-    "revision": "24260eaa47040d28848c",
+    "revision": "35502aa64f863a6eb807",
     "url": "/portfolio/static/css/main.53b2a233.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = [
     "url": "/portfolio/static/media/mail.c86b59ba.svg"
   },
   {
-    "revision": "24260eaa47040d28848c",
-    "url": "/portfolio/static/js/main.79d67cf2.chunk.js"
+    "revision": "35502aa64f863a6eb807",
+    "url": "/portfolio/static/js/main.4bcfbe37.chunk.js"
   },
   {
     "revision": "de1216cc3f18b835824c2aeb75f4e440",
@@ -40,7 +40,7 @@ self.__precacheManifest = [
     "url": "/portfolio/static/media/uiux.cfe83b5f.mov"
   },
   {
-    "revision": "5ec3f800c5bf7459f9caf0612719d915",
+    "revision": "000800a610530178c31dbcc9c5d0eb6e",
     "url": "/portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.9c9e59fff1f892be90674ec19a16487b.js"
+  "/portfolio/precache-manifest.e8f46e99199118c2ffe5772544f8a058.js"
 );
 
 workbox.clientsClaim();
