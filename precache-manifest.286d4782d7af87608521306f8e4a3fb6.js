f0261c7c59eccc6419b8596b520e9789127e7cff
guer0157 <32390693+guer0157@users.noreply.github.com>
2020-01-14 22:31:19 -0500

    Updates

---
+++ precache-manifest.286d4782d7af87608521306f8e4a3fb6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf7f35d53140f94242285da20208ecaa",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "09d37fa2549ddb3a1879",
+    "url": "/portfolio/static/css/main.fc2bad2d.chunk.css"
+  },
+  {
+    "revision": "8775563f26236cae13d4",
+    "url": "/portfolio/static/js/2.aabefd6c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.aabefd6c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "09d37fa2549ddb3a1879",
+    "url": "/portfolio/static/js/main.66b34aa2.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "0e7cbb457656df15ab53d81f723416dd",
+    "url": "/portfolio/static/media/MyMovie.0e7cbb45.mp4"
+  },
+  {
+    "revision": "3b262eafeeefe2afbb36d6033442ee81",
+    "url": "/portfolio/static/media/ace.3b262eaf.mov"
+  },
+  {
+    "revision": "b37146e31033ec9eff5e7119fbd235db",
+    "url": "/portfolio/static/media/iosdevelopment.b37146e3.mov"
+  },
+  {
+    "revision": "de1216cc3f18b835824c2aeb75f4e440",
+    "url": "/portfolio/static/media/linkedin.de1216cc.svg"
+  },
+  {
+    "revision": "c86b59baaffcff34f61c525e11d3a004",
+    "url": "/portfolio/static/media/mail.c86b59ba.svg"
+  },
+  {
+    "revision": "dca81bfb31d6c6692b6cfee7f3e1f668",
+    "url": "/portfolio/static/media/tel.dca81bfb.svg"
+  },
+  {
+    "revision": "cfe83b5f0302e42d14446b45408b53de",
+    "url": "/portfolio/static/media/uiux.cfe83b5f.mov"
+  }
+]);
